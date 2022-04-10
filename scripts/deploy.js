f7758c67af156569b2706aa608020ead7de91f42
emremre <emre.guclu@metu.edu.tr>
2022-04-10 16:25:37 +0300

    first commit that produces onchain nft from svg

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('malumnft');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+  
+    // Call the function.
+    let txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+    console.log("malumun ilani")
+  
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
